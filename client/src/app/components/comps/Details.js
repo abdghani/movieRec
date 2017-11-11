d1ea06a39c52619199af2a4906622c41f6fe4f1d
abdghani995 <abdullah.ghani3@gmail.com>
2017-11-11 20:59:54 +0530

    ui changes

--- client/src/app/components/comps/Details.js
+++ client/src/app/components/comps/Details.js
@@ -1,17 +1,22 @@
 
 import React, { Component } from 'react';
-import { Grid, Menu, Segment } from 'semantic-ui-react'
+import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react'
+import { Link, Route, Switch ,Redirect} from 'react-router-dom';
 import apiService from '../../service/api.service.js'
 var request = require('superagent');
+
 
 
 class Details extends Component {
   constructor(props) {
     super(props);
-    this.state = { counter: 1 ,genres:[]};
+    this.state = {
+      genres:[],
+      visible: true
+    };
   }
 
-  componentWillMount() {
+  componentDidMount() {
     let self = this;
     apiService
     .get('/genres')
@@ -19,31 +24,43 @@ class Details extends Component {
       self.setState({
         genres:res
       })
-      console.log(self.state)
     })
   }
-
-  /*let handleItemClick : (e, { name }) => this.setState({ activeItem: name }),*/
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
 
   render(){
-    const { activeItem } = this.state;
+    const { visible } = this.state
       return (
-            <Grid>
-            <Grid.Column width={4}>
-              <Menu fluid vertical tabular>
-                <Menu.Item name='bio' active={activeItem === 'bio'} onClick={this.handleItemClick} />
-                <Menu.Item name='pics' active={activeItem === 'pics'} onClick={this.handleItemClick} />
-                <Menu.Item name='companies' active={activeItem === 'companies'} onClick={this.handleItemClick} />
-                <Menu.Item name='links' active={activeItem === 'links'} onClick={this.handleItemClick} />
-              </Menu>
-            </Grid.Column>
-            <Grid.Column stretched width={12}>
-              <Segment>
-                This is an stretched grid column. This segment will always match the tab height
-              </Segment>
-            </Grid.Column>
-            </Grid>
-      )
+              <div className="mainHeight">
+                <Sidebar.Pushable as={Segment}>
+
+                  <Sidebar as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
+                    <Link to="/home">
+                      <Menu.Item name='movie'>
+                        <Icon name='film' />
+                        MOVIE
+                      </Menu.Item>
+                    </Link>
+                    <Link to="/tasks">
+                      <Menu.Item name='genre'>
+                        <Icon name='tasks' />
+                        GENRE
+                      </Menu.Item>
+                    </Link>
+                  </Sidebar>
+
+                  <Sidebar.Pusher>
+                    <Segment basic>
+                        <Route exact path="/" render={() => (<Redirect to="/home"/>)}/>
+                        <Route exact path="/home" component={Home}/>
+                        <Route path="/category" component={Category}/>
+                        <Route path="/products" component={Products}/>
+                    </Segment>
+                  </Sidebar.Pusher>
+                </Sidebar.Pushable>
+              </div>
+    )
+
   }
 }
 export default Details
